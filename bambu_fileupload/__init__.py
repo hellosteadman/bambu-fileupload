4ef150a2fdec8a70c71b7f87e3d66f16d0767895
Mark Steadman <mark@steadman.io>
2016-04-02 15:35:23 +0100

    Remove url.patterns

--- bambu_fileupload/__init__.py
+++ bambu_fileupload/__init__.py
@@ -1,7 +1,7 @@
 from django.core.files import File
 from django.http import HttpRequest
 
-__version__ = '3.1'
+__version__ = '3.2'
 
 DEFAULT_HANDLERS = (
     (
--- bambu_fileupload/urls.py
+++ bambu_fileupload/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from bambu_fileupload.views import upload, delete, filelist
 
-urlpatterns = patterns('bambu_fileupload.views',
-    url(r'^$', 'upload', name = 'fileupload'),
-    url(r'^delete/$', 'delete', name = 'fileupload_delete'),
-    url(r'^list/$', 'filelist', name = 'fileupload_list')
+urlpatterns = (
+    url(r'^$', upload, name = 'fileupload'),
+    url(r'^delete/$', delete, name = 'fileupload_delete'),
+    url(r'^list/$', filelist, name = 'fileupload_list')
 )
